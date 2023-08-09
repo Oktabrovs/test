c8190ba55c9ed0a5bf1e089bee3393481ff5f0cb
Sukhrob Yangibaev <sukhrobyangibaev@gmail.com>
2023-08-09 20:33:14 +0500

    fix

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from os import environ
 
 from telegram import Update
-from telegram.ext import Application, PicklePersistence, ContextTypes, CommandHandler
+from telegram.ext import Application, PicklePersistence, ContextTypes, CommandHandler, MessageHandler, filters
 
 
 async def start(update: Update, _) -> None:
     await update.message.reply_text('Welcome to the code checker bot!')
+
+
+async def code_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    main_code_string: str = ''
+
+    print(update)
 
 
 def main() -> None:
@@ -14,6 +20,7 @@ def main() -> None:
     app = Application.builder().token(environ['TOKEN']).persistence(persistence).build()
 
     app.add_handler(CommandHandler('start', start))
+    app.add_handler(MessageHandler((filters.TEXT | filters.Document.PY) & ~filters.COMMAND, code_handler))
 
     app.run_polling()
 
